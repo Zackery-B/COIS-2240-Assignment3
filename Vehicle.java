e818efe5f7908f3488a6fb68f09970495220cc7a
Zack <06zackery@gmail.com>
2025-04-03 21:29:27 -0400

    make helper method and call from constructor

--- Vehicle.java
+++ Vehicle.java
@@ -8,20 +8,21 @@ public abstract class Vehicle {
     public enum VehicleStatus { AVAILABLE, RESERVED, RENTED, MAINTENANCE, OUTOFSERVICE }
 
     public Vehicle(String make, String model, int year) {
-    	if (make == null || make.isEmpty())
-    		this.make = null;
-    	else
-    		this.make = make.substring(0, 1).toUpperCase() + make.substring(1).toLowerCase();
-    	
-    	if (model == null || model.isEmpty())
-    		this.model = null;
-    	else
-    		this.model = model.substring(0, 1).toUpperCase() + model.substring(1).toLowerCase();
-    	
+    	this.make = capitalize(make);
+    	this.model = capitalize(model);
         this.year = year;
         this.status = VehicleStatus.AVAILABLE;
         this.licensePlate = null;
     }
+    
+    // helper method to avoid repeat code 
+    private String capitalize(String input) {
+    	if (input == null || input.isEmpty())
+    		return null;
+    	else
+    		return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
+    }
+    
 
     public Vehicle() {
         this(null, null, 0);
